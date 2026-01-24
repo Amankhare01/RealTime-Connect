2dcb1468635b5e7efeaf2b0662c10c63cd55226f
Amankhare01 <amankhare.aa@gmail.com>
2026-01-24 14:00:01 +0530

    instant message

--- lib/socketClient.ts
+++ lib/socketClient.ts
@@ -4,10 +4,10 @@ let socket: Socket | null = null;
 
 export function getSocket(): Socket {
   if (!socket) {
-socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-  transports: ["websocket"],
-});
-
+    socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
+      transports: ["websocket"],
+      reconnection: true,
+    });
   }
   return socket;
 }
